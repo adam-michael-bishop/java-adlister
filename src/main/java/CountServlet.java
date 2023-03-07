d9b2d60760ff697ede58f704c8fca22476f2b561
Adam Bishop <adam.michael.bishop@gmail.com>
2023-03-07 13:39:27 -0600

    completed servlets exercise

---
+++ src/main/java/CountServlet.java
@@ -0,0 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "CountServlet", urlPatterns = "/count")
+public class CountServlet extends HttpServlet {
+    private int count = 0;
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getParameter("reset") != null && req.getParameter("reset").equals("true")) {
+            count = 0;
+        }
+        count++;
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("This page has been visited " + count + " time(s)");
+    }
+}
