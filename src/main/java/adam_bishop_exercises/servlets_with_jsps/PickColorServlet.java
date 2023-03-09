97924eee17267108fb1100476aefe18be05b59c9
Adam Bishop <adam.michael.bishop@gmail.com>
2023-03-09 12:04:15 -0600

    added pick color stuff

---
+++ src/main/java/adam_bishop_exercises/servlets_with_jsps/PickColorServlet.java
@@ -0,0 +1,9 @@
+package adam_bishop_exercises.servlets_with_jsps;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "PickColorServlet", urlPatterns = "/pickcolor")
+public class PickColorServlet extends HttpServlet {
+
+}
---
+++ src/main/webapp/servlets_with_jsps/pick-color.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<%@include file="../partials/head.jsp"%>
+<body>
+<%@include file="../partials/navbar.jsp"%>
+
+<%@include file="../partials/scripts.jsp"%>
+</body>
+</html>
